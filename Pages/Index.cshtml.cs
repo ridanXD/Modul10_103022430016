ee1db1225761d5e3375192638dfc07cdd5b9a24d
ridanXD <rizkyaramdancringe@gmail.com>
2026-05-04 00:36:14 +0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Modul10_103022430016.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
